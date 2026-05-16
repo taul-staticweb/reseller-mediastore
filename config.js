218d50324b923564961c6a5e4d6d93fc3f4e4e82
taul-staticweb <mmiftahulrizqi28@gmail.com>
2026-05-16 09:35:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * CONFIG — Ganti URL ini saat deploy untuk klien baru
  * ============================================================
  */
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyTB7L7n7La5f5tzUGYD1rRd_p0RDfO6pW4IHP7oE4FuSMlSTpizCZ_pKfdhtMZvA/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbytgJWxL8V5WF_NqHQfX3v4th2QOzW1Hd_MYWPlioD0tDl_QFphLcHkDmAMk0yjK6do/exec';
