a533af0008ab5188ad6dc8b5f312e1668867232e
taul-staticweb <mmiftahulrizqi28@gmail.com>
2026-05-16 09:34:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+/**
+ * ============================================================
+ * CONFIG — Ganti URL ini saat deploy untuk klien baru
+ * ============================================================
+ */
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyTB7L7n7La5f5tzUGYD1rRd_p0RDfO6pW4IHP7oE4FuSMlSTpizCZ_pKfdhtMZvA/exec';
